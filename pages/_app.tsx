df9265de02ca9e493c5e6fcc6aa760537e58ec71
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-24 17:39:10 +0100

    feat: add animate on scroll library and animate.css
    Enhances UX as user gets to see some animations while browsing the pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,38 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'animate.css'
+// fonts
+import '@fontsource/roboto/700.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/manjari';
+
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import React from 'react';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    AOS.init({
+      // easing: 'ease-out-cubic',
+      once: false,
+      offset: 120,
+      startEvent: 'DOMContentLoaded',
+      // placement: 'top-top',
+      // offset: 50,
+      duration: 1000,
+      // easing: 'ease-in-sine',
+      delay: 400,
+      mirror: true,
+    });
+  }, []);
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <ProSidebarProvider>
+        <Component {...pageProps} />
+      </ProSidebarProvider>
     </ChakraProvider>
   );
 }
