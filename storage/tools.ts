e8cb4c3d0b3f8991594fba7b6cac2a82e780f6ca
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-24 17:51:46 +0100

    mock tools for project

---
+++ storage/tools.ts
@@ -0,0 +1,197 @@
+import type { ITool } from "interfaces/tool";
+
+const MockTools: ITool[] = [
+  
+  {
+    name: 'tool-1',
+    title: 'Tool 1',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Aliqua ut sint irure sit. Ullamco aute labore labore cupidatat. Eu reprehenderit elit tempor labore et qui enim quis veniam officia. Velit tempor in velit ex ullamco ea dolore labore tempor aliqua ex reprehenderit duis.',
+    url: 'https://tool1.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel', 'manuel'],
+    category: 'Category 1'
+  },
+  {
+    name: 'tool-2',
+    title: 'Tool 2',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Laborum reprehenderit ullamco elit proident commodo consectetur anim voluptate dolore nulla. Ad consectetur fugiat magna consectetur quis ex adipisicing deserunt est sint irure. Exercitation enim aliqua elit aliquip. Qui est dolore esse veniam ipsum voluptate eiusmod tempor aute ipsum sit. Nisi voluptate qui dolor in ea enim cupidatat cupidatat ad ex nisi incididunt. Do nulla mollit aliquip culpa. Laborum ea aliquip eu velit et quis excepteur.',
+    url: 'https://tool2.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 2'
+  },
+  {
+    name: 'tool-3',
+    title: 'Tool 3',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Aliqua ut sint irure sit. Ullamco aute labore labore cupidatat. Eu reprehenderit elit tempor labore et qui enim quis veniam officia. Velit tempor in velit ex ullamco ea dolore labore tempor aliqua ex reprehenderit duis.',
+    url: 'https://tool1.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel', 'manuel'],
+    category: 'Category 3'
+  },
+  {
+    name: 'tool-4',
+    title: 'Tool 4',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Laborum reprehenderit ullamco elit proident commodo consectetur anim voluptate dolore nulla. Ad consectetur fugiat magna consectetur quis ex adipisicing deserunt est sint irure. Exercitation enim aliqua elit aliquip. Qui est dolore esse veniam ipsum voluptate eiusmod tempor aute ipsum sit. Nisi voluptate qui dolor in ea enim cupidatat cupidatat ad ex nisi incididunt. Do nulla mollit aliquip culpa. Laborum ea aliquip eu velit et quis excepteur.',
+    url: 'https://tool2.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 1'
+  },
+  {
+    name: 'tool-5',
+    title: 'Tool 5',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Aliqua ut sint irure sit. Ullamco aute labore labore cupidatat. Eu reprehenderit elit tempor labore et qui enim quis veniam officia. Velit tempor in velit ex ullamco ea dolore labore tempor aliqua ex reprehenderit duis.',
+    url: 'https://tool1.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel', 'manuel'],
+    category: 'Category 2'
+  },
+  {
+    name: 'tool-6',
+    title: 'Tool 6',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Laborum reprehenderit ullamco elit proident commodo consectetur anim voluptate dolore nulla. Ad consectetur fugiat magna consectetur quis ex adipisicing deserunt est sint irure. Exercitation enim aliqua elit aliquip. Qui est dolore esse veniam ipsum voluptate eiusmod tempor aute ipsum sit. Nisi voluptate qui dolor in ea enim cupidatat cupidatat ad ex nisi incididunt. Do nulla mollit aliquip culpa. Laborum ea aliquip eu velit et quis excepteur.',
+    url: 'https://tool2.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 3'
+  },
+  {
+    name: 'tool-7',
+    title: 'Tool 7',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Ut est ex do laborum nulla exercitation quis enim culpa anim ullamco ea sint pariatur. Excepteur occaecat aute est veniam. Ipsum et proident mollit eu.',
+    url: 'https://tool3.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 1'
+  },
+  {
+    name: 'tool-8',
+    title: 'Tool 8',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Occaecat ex elit veniam occaecat elit excepteur esse reprehenderit ex. Ea laborum Lorem proident ea aliqua magna excepteur cupidatat excepteur aute elit mollit eu. Ullamco culpa ullamco nulla consequat incididunt. Quis quis mollit duis eu elit labore exercitation consequat do irure eiusmod dolore voluptate Lorem.',
+    url: 'https://tool4.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 2'
+  },
+  {
+    name: 'tool-9',
+    title: 'Tool 9',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Ut est ex do laborum nulla exercitation quis enim culpa anim ullamco ea sint pariatur. Excepteur occaecat aute est veniam. Ipsum et proident mollit eu.',
+    url: 'https://tool3.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 3'
+  },
+  {
+    name: 'tool-10',
+    title: 'Tool 10',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Occaecat ex elit veniam occaecat elit excepteur esse reprehenderit ex. Ea laborum Lorem proident ea aliqua magna excepteur cupidatat excepteur aute elit mollit eu. Ullamco culpa ullamco nulla consequat incididunt. Quis quis mollit duis eu elit labore exercitation consequat do irure eiusmod dolore voluptate Lorem.',
+    url: 'https://tool4.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 1'
+  },
+  {
+    name: 'tool-11',
+    title: 'Tool 11',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Ut est ex do laborum nulla exercitation quis enim culpa anim ullamco ea sint pariatur. Excepteur occaecat aute est veniam. Ipsum et proident mollit eu.',
+    url: 'https://tool3.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 2'
+  },
+  {
+    name: 'tool-12',
+    title: 'Tool 12',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Occaecat ex elit veniam occaecat elit excepteur esse reprehenderit ex. Ea laborum Lorem proident ea aliqua magna excepteur cupidatat excepteur aute elit mollit eu. Ullamco culpa ullamco nulla consequat incididunt. Quis quis mollit duis eu elit labore exercitation consequat do irure eiusmod dolore voluptate Lorem.',
+    url: 'https://tool4.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 3'
+  },
+  {
+    name: 'tool-13',
+    title: 'Tool 13',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit. Culpa esse commodo Lorem ullamco. Culpa mollit labore dolore officia ipsum ad incididunt elit excepteur culpa proident ex fugiat ad. Sunt ex labore ut labore quis dolore veniam dolor in consectetur adipisicing veniam adipisicing.',
+    url: 'https://tool5.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 1'
+  },
+  {
+    name: 'tool-14',
+    title: 'Tool 14',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Cupidatat ipsum labore et proident anim veniam duis tempor ea. Veniam est eu id occaecat et Lorem eu ad fugiat occaecat. Pariatur velit occaecat laboris esse nisi duis incididunt commodo Lorem duis esse. Id mollit elit ex ad minim aliquip nulla adipisicing reprehenderit in. Amet commodo laborum non quis excepteur laboris anim non excepteur duis.',
+    url: 'https://tool6.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 2'
+  },
+  {
+    name: 'tool-15',
+    title: 'Tool 15',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit. Culpa esse commodo Lorem ullamco. Culpa mollit labore dolore officia ipsum ad incididunt elit excepteur culpa proident ex fugiat ad. Sunt ex labore ut labore quis dolore veniam dolor in consectetur adipisicing veniam adipisicing.',
+    url: 'https://tool5.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 3'
+  },
+  {
+    name: 'tool-16',
+    title: 'Tool 16',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Cupidatat ipsum labore et proident anim veniam duis tempor ea. Veniam est eu id occaecat et Lorem eu ad fugiat occaecat. Pariatur velit occaecat laboris esse nisi duis incididunt commodo Lorem duis esse. Id mollit elit ex ad minim aliquip nulla adipisicing reprehenderit in. Amet commodo laborum non quis excepteur laboris anim non excepteur duis.',
+    url: 'https://tool6.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 1'
+  },
+  {
+    name: 'tool-17',
+    title: 'Tool 17',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit. Culpa esse commodo Lorem ullamco. Culpa mollit labore dolore officia ipsum ad incididunt elit excepteur culpa proident ex fugiat ad. Sunt ex labore ut labore quis dolore veniam dolor in consectetur adipisicing veniam adipisicing.',
+    url: 'https://tool5.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 2'
+  },
+  {
+    name: 'tool-18',
+    title: 'Tool 18',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Cupidatat ipsum labore et proident anim veniam duis tempor ea. Veniam est eu id occaecat et Lorem eu ad fugiat occaecat. Pariatur velit occaecat laboris esse nisi duis incididunt commodo Lorem duis esse. Id mollit elit ex ad minim aliquip nulla adipisicing reprehenderit in. Amet commodo laborum non quis excepteur laboris anim non excepteur duis.',
+    url: 'https://tool6.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 3'
+  },
+  {
+    name: 'new-tool',
+    title: 'New Tool',
+    shortDescription: 'A short and brief Description, like Lorem nostrud in enim Lorem esse cillum excepteur sunt deserunt reprehenderit.',
+    longDescription: 'A long description like Cupidatat ipsum labore et proident anim veniam duis tempor ea. Veniam est eu id occaecat et Lorem eu ad fugiat occaecat. Pariatur velit occaecat laboris esse nisi duis incididunt commodo Lorem duis esse. Id mollit elit ex ad minim aliquip nulla adipisicing reprehenderit in. Amet commodo laborum non quis excepteur laboris anim non excepteur duis.',
+    url: 'https://newtool.com',
+    tool_type: 'template',
+    authors: ['mofor emmanuel'],
+    category: 'Category 3'
+  }
+]
+
+export {MockTools}
