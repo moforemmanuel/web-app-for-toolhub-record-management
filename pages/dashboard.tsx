7071a0c6f3a4238bb13f859bea6792751b9a9b15
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-24 17:42:29 +0100

    feat: build dashboard page
    the dashboard contains information and vital statistics and metrics about the user's activities, contributions, as well as record statistics

---
+++ pages/dashboard.tsx
@@ -0,0 +1,99 @@
+import FullPageLoader from '@/components/fullPageLoader/FullPageLoader';
+import Layout from '@/components/Layout/Layout';
+import LeftDash from '@/components/LeftDash/LeftDash';
+import MiddleDash from '@/components/MiddleDash/MiddleDash';
+import RightDash from '@/components/RightDash/RightDash';
+import {
+  Avatar,
+  Box,
+  Center,
+  Flex,
+  Link,
+  GridItem,
+  Heading,
+  HStack,
+  SimpleGrid,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { Menu, MenuItem, Sidebar, useProSidebar } from 'react-pro-sidebar';
+import { MockUsers } from 'storage/users';
+
+const Dashboard = () => {
+  const router = useRouter();
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  const { collapseSidebar } = useProSidebar();
+
+  const user = MockUsers.find((user) => user.username === 'moforemmanuel');
+
+  React.useEffect(() => {
+    setIsMounted(true);
+
+    // reload on resize to fix responsive layout issues
+    window.addEventListener('resize', () => {
+      window.location.reload();
+    });
+  }, []);
+
+  if (!isMounted) {
+    return <FullPageLoader />;
+  }
+  return (
+    isMounted && (
+      <Layout>
+        <Center minH="82vh" minW="100%" maxH="100%" maxW="100%" bg="gray.100">
+          {!user ? (
+            <Heading>No User Found</Heading>
+          ) : (
+            <Flex
+              align="flex-start"
+              justify="flex-start"
+              direction="row"
+              gap={6}
+              maxW="100%"
+              minW="100%"
+              p={3}
+            >
+              {/* Sidebar menu */}
+
+              {/* <LeftDash user={user} /> */}
+
+              {/* Sidebar menu */}
+
+              <SimpleGrid maxW="100%" minW="100%" columns={5} m={0} spacing={3}>
+                <GridItem
+                  colSpan={{ base: 5, lg: 4 }}
+                  // border="thin solid red"
+                  // bg="white"
+                >
+                  {/* Sidebar menu */}
+
+                  <MiddleDash user={user} />
+
+                  {/* Sidebar menu */}
+                </GridItem>
+
+                <GridItem
+                  colSpan={{ base: 5, lg: 1 }}
+                  // border="thin solid green"
+                  bg="rgb(241,240,241)"
+                  shadow="md"
+                  rounded="lg"
+                >
+                  {/* Sidebar menu */}
+
+                  <RightDash user={user} />
+
+                  {/* Sidebar menu */}
+                </GridItem>
+              </SimpleGrid>
+            </Flex>
+          )}
+        </Center>
+      </Layout>
+    )
+  );
+};
+
+export default Dashboard;
